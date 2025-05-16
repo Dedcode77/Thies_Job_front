dd968f5b1c565613a39cb4ea5d73752e6af24643
Dedcode77 <salifciss222@gmail.com>
2025-05-16 16:08:37 +0200

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
-
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default App
-=======
 import React from "react";
 
 export default function App() {
   return (
     <div className="bg-gray-400">
-      <h1>Bonjour</h1>
+      <h1>Bonjour tres chers</h1>
     </div>
   );
 }
->>>>>>> a02fc5f79489723262d6dfc5789a306625b1ead4
