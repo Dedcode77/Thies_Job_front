e589be28efb766d5fa9da4dabec3163fdd258758
Dedcode77 <salifciss222@gmail.com>
2025-05-17 14:36:50 +0200

    jour 2

---
+++ src/pages/Communautes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Communautes() {
+  return (
+    <div>
+      <h1>Communautes</h1>
+    </div>
+  )
+}
+
+export default Communautes
--- src/pages/auth/Inscription.jsx
+++ src/pages/auth/Inscription.jsx
@@ -6,6 +6,8 @@ function Inscription() {
     nom: "",
     email: "",
     password: "",
+    confirmPassword: "",
+    genre: "",
   });
 
   const handleChange = (e) => {
@@ -18,7 +20,7 @@ function Inscription() {
     e.preventDefault();
 
     alert(
-      `Nom: ${form.nom}\nEmail: ${form.email}\nMot de passe: ${form.password}`
+      `Nom: ${form.nom}\nEmail: ${form.email}\nMot de passe: ${form.password}\nConfirmer le mot de passe: ${form.confirmPassword}\nGenre: ${form.genre}`
     );
 
     navigate("/dashboard");
@@ -28,7 +30,7 @@ function Inscription() {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <form
         onSubmit={handleSubmit}
-        className="bg-gray-400 p-8 rounded shadow-md w-96"
+        className="bg-blue-900 p-8 rounded shadow-md w-96 text-white"
       >
         <h1 className="text-2xl font-bold mb-6 text-center">Inscription</h1>
         <div className="mb-4">
@@ -63,6 +65,31 @@ function Inscription() {
             className="w-full border px-3 py-2 rounded"
             required
           />
+        </div>
+        <div className="mb-6">
+          <label className="block mb-1">Confirmer le mot de passe</label>
+          <input
+            type="password"
+            name="password"
+            value={form.confirmPassword}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 rounded"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block mb-1">Genre</label>
+          <select
+            name="genre"
+            value={form.genre}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 rounded"
+            required>
+            <option value="">Sélectionner un genre</option>
+            <option value="homme">Homme</option>
+            <option value="femme">Femme</option>
+            </select>
+
         </div>
         <button
           type="submit"
